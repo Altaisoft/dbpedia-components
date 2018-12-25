667d75de86cd4a87b1bbc1604675132493edabe3
anatoly.scherbakov <altaisoft@gmail.com>
2018-12-25 20:51:29 +0700

    Changed query

--- dbpedia-timeline/dbpedia-category-table.js
+++ dbpedia-timeline/dbpedia-category-table.js
@@ -1,21 +1,22 @@
 var query = `
-PREFIX : <http://iolanta.tech/>
+PREFIX : <http://www.ontotext.com/plugins/geosparql#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX dbr: <http://dbpedia.org/resource/>
+PREFIX schema: <http://schema.org/>
 PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX dbc: <http://dbpedia.org/resource/Category:>
 PREFIX dct: <http://purl.org/dc/terms/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX prov: <http://www.w3.org/ns/prov#>
 
-
-select ?resource ?label ?link ?thumbnail where {
-    ?resource dct:subject dbc:Battles_of_World_War_I_involving_Germany .
+select ?resource ?label ?date ?link where {
+    ?resource dct:subject dbc:Battles_of_World_War_I_involving_Belgium .
     ?resource rdfs:label ?label .
-    ?resource foaf:isPrimaryTopicOf ?link .
-    ?resource dbo:thumbnail ?thumbnail .
+    ?resource dbo:date ?date .
+    ?resource prov:wasDerivedFrom ?link .
     
     FILTER (lang(?label) = 'en') .
-} ORDER BY ?label limit 5
+} ORDER BY ?date limit 50
 `
 
 var url = new URL('https://dbpedia.org/sparql'),
@@ -32,6 +33,7 @@ Object.keys(params).forEach(
 fetch(url).then(function(response) {
     response.json().then(function(data) {
         var items = data.results.bindings.map(function(item) { return {
+            date: item.date.value,
             label: item.label.value,
             href: item.link.value,
         }});
@@ -39,10 +41,11 @@ fetch(url).then(function(response) {
 
         var table = new Tabulator("#table", {
             height: 300,
-            data: items, //assign data to table
-            layout: "fitColumns", //fit columns to width of table (optional)
-            columns: [ //Define Table Columns
-                {title: "Page", field: "label"},
+            data: items,
+            layout: "fitColumns",
+            columns: [
+                {title: "Date", field:"date", align:"left", width: 150},
+                {title: "Event", field: "label"},
             ],
             rowClick: function(e, row) {
                 window.open(row._row.data.href)
